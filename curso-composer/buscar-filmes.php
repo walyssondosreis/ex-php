67eb8d0db5cf231218271486bb6c1ab6ff661872
Walysson dos Reis <walyssondosreis@email.com>
2022-08-01 15:53:06 -0300

    teste

--- curso-composer/buscar-filmes.php
+++ curso-composer/buscar-filmes.php
@@ -1,8 +1,10 @@
 <?php
 
-require_once "vendor/autoload.php";
+
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
+
+require_once "vendor/autoload.php";
 
 $client = new Client();
 $resposta = $client->request('GET','https://filmow.com/listas/documentarios-que-vao-expandir-sua-visao-do-mundo-l10957/');
